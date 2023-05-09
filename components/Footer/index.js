ffc826e2ab567527de9ad99f2e83bc9107a9e2b2
Binil B <binil.b.re@gmail.com>
2023-05-09 11:41:57 +0530

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -11,10 +11,9 @@ const Footer = ({}) => {
           <h1 className="text-2xl text-bold">Contact.</h1>
           <div className="mt-10">
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              LET&apos;S WORK
+              CONNECT WITH  ME
             </h1>
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
-              TOGETHER
             </h1>
             <Button type="primary">Lets Connect</Button>
             <div className="mt-10">
